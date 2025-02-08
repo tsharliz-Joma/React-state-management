8ff8f7a7c1d482089daa0439d2c9ef5c62b6c279
charles joma <charles.joma@chepnetwork.com>
2025-02-08 15:25:00 +1100

    MOVE :: move back to personal laptop

---
+++ contextapi/src/context.js
@@ -0,0 +1,9 @@
+import { createContext } from "react";
+
+const Context = createContext();
+
+const Provider = ({ children }) => {
+  return <Context.Provider value={{}}>{children}</Context.Provider>;
+};
+
+export default Provider;
